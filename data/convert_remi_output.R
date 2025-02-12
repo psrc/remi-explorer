69813eaf9d697e4b73f53048d3c1adf7a542cca4
Hana Sevcikova <hsevcikova@psrc.org>
2025-02-12 11:33:59 -0800

    changed names of most scenarios

--- data/convert_remi_output.R
+++ data/convert_remi_output.R
@@ -2,10 +2,10 @@ library(data.table)
 library(readxl)
 
 # what should this scenario be called
-scenario.name <- "my_scenario" 
+scenario.name <- "LUVit_emp_cnty_adj_mig" 
 
 # name of the Excel file containing REMI results, exported directly from REMI
-remi.results.file <- "~/psrc/REMI/export/Template_Dashboard_Indicators.xlsx"
+remi.results.file <- "~/T/2025Q1/Hana/REMI/Dashboard_Indicators_LUVitEmpSecCntyAltMigSpeedAdj.xlsx"
 
 # names of the individual sheets that we want to include in the resulting dataset
 # and the corresponding value in the "Main Measure" column 

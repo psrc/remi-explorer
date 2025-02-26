758f4ec79bf21dacab9588d2c23875551dc9fa19
Hana Sevcikova <hanas@uw.edu>
2025-02-26 11:31:43 -0800

    state-level script finished

--- data/acs_householders_wa.R
+++ data/acs_householders_wa.R
@@ -4,9 +4,10 @@ library(dplyr)
 library(data.table)
 library(magrittr)
 
-age_categories <- c("under 5 years", "5 to 9 years", "10 to 14 years", "15 to 24 years", 
-                    "25 to 34 years", "35 to 44 years", "45 to 54 years", "55 to 59 years", 
-                    "60 to 64 years", "65 to 74 years", "75 to 84 years", "85 years and over")
+age_categories <- c("Under 15 years", "15 to 24 years", 
+                    "25 to 34 years", "35 to 44 years", "45 to 54 years", 
+                    "55 to 64 years", "65 to 74 years", "75 to 84 years", "85 years and over")
+
 
 safe_rgx_extract <- function(var, rgx){
     val <- if_else(str_detect(var, rgx), str_replace(str_extract(var, rgx), rgx, "\\1"), "")
@@ -19,21 +20,28 @@ age_lookup <- suppressWarnings(data.table(
     age_max=coalesce(as.integer(safe_rgx_extract(age_categories, "(\\d+) years$")), 120)
 ))
 
+# assigns min and max of the corresponding age range
+assign_age_range <- function(dt){
+    dt[grepl("^Under ", age),  `:=`(age1=as.integer(safe_rgx_extract(age, "(?:Under )(\\d+)")) -1,
+                                     age2=as.integer(safe_rgx_extract(age, "(?:Under )(\\d+)")) -1)]
+    dt[!grepl("^Under ", age), `:=`(age1=as.integer(safe_rgx_extract(age, "(\\d+)")),
+                                     age2=as.integer(safe_rgx_extract(age, "(\\d+)(?: years)?(?: and over)?$")))] 
+}
+
 # Helper function
 sum_by_age <- function(dt){
     dtx <- setDT(dt) %>% .[grepl('years', label)] %>%
-        .[, `:=`(age =str_extract(label, "(?<=!!)[\\w ]+$"))] %>%                  # Separate age from label components]  
-        .[, `:=`(age1=as.integer(safe_rgx_extract(age, "(?:Under )?(\\d+)")),
-                 age2=as.integer(safe_rgx_extract(age, "(\\d+)(?: years)?(?: and over)?$")))] %>% 
-        .[age_lookup, age:=age_label, on=.(age1 >= age_min, age2 <= age_max)]  # Lookup standard age categories
-    
+        .[, `:=`(age =str_extract(label, "(?<=!!)[\\w ]+$"))]                    # Separate age from label components]
+    assign_age_range(dtx)
+    dtx[age_lookup, age:=age_label, on=.(age1 >= age_min, age2 <= age_max)]   # Recode using standard age categories
+   
     dtx[!is.na(age), 
               .(sum_estimate=sum(estimate), 
                 sum_moe=tidycensus::moe_sum(moe, estimate, na.rm=TRUE)),         # Summarize
               by=c("year", "age")]
 } 
 
-householder <- gq <- tot_pop <- NULL
+householder <- hh_pop <- tot_pop <- NULL
 for(yr in c(2020, 2023)){
     householder <- rbind(householder, 
                          tidycensus::get_acs(state = "Washington", geography = "state", 
@@ -42,7 +50,7 @@ for(yr in c(2020, 2023)){
         psrccensus:::label_acs_variables("B25007", yr, "acs5") %>% 
             setDT() %>% .[, year := yr]
     )
-    gq <- rbind(gq,
+    hh_pop <- rbind(hh_pop,
                     tidycensus::get_acs(state = "Washington", geography = "state", 
                                         year = yr, survey = "acs5", 
                                         table = "B26101", cache_table = TRUE)  %>% 
@@ -50,16 +58,28 @@ for(yr in c(2020, 2023)){
                         .[, subvar:=as.integer(stringr::str_extract(variable,"(?<=_)\\d+$"))] %>%    
                         .[subvar %in% c(2:11, 35:44)] %>% .[, year := yr]
                     )
-    tot_pop <- rbind(tot_pop, 
-                         tidycensus::get_acs(state = "Washington", geography = "state", 
-                                             year = yr, survey = "acs5", 
-                                             table = "B01001", cache_table = TRUE)  %>% 
-                             psrccensus:::label_acs_variables("B01001", yr, "acs5") %>% 
-                             setDT() %>% .[, year := yr]
-                    )
 }
-
+# sum for selected age groups
 householder_sum <- sum_by_age(householder)
-gq_sum <- sum_by_age(gq)
-hh_pop <- sum_by_age(tot_pop)
-hh_pop[gq_sum, sum_estimate := sum_estimate - i.sum_estimate, on = .(year, age)]
+hh_pop[between(subvar, 35, 44), estimate:=estimate * -1]        # HH pop is total - GQ
+hh_pop_sum <- sum_by_age(hh_pop)
+tot_pop_sum <- sum_by_age(hh_pop[estimate > 0])
+
+# assemble final table
+output_table <- merge(merge(householder_sum[, .(year, age, householders = sum_estimate)],
+                            hh_pop_sum[, .(year, age, HHpop = sum_estimate)],
+                                by = c("year", "age"), all = TRUE),
+                      tot_pop_sum[, .(year, age, Pop = sum_estimate)],
+                            by = c("year", "age"), all = TRUE
+                        )
+output_table[is.na(householders), householders := 0]
+
+# re-write age groups as intervals
+assign_age_range(output_table)
+output_table[age1 == min(age1), age1 := 0]
+output_table[, age := paste(age1, age2, sep = "-")]
+output_table[age2 == max(age2), age:= paste0(age1, "+")]
+output_table <- output_table[order(year, age1)][, `:=`(age1 = NULL, age2 = NULL)]
+
+# export
+fwrite(output_table, file = "acs_hhpop_by_age.csv")
